e8ee24d5eeaee24ff3f1280d2e07f4a11151d5a7
Lawson Matutu <lawsonmatutu@gmail.com>
2024-02-04 09:53:41 +0200

    updating

--- src/App.js
+++ src/App.js
@@ -7,12 +7,21 @@ import Projects from "./components/projects/Projects";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
 import FooterBottom from "./components/footer/FooterBottom";
+import { Helmet } from "react-helmet";
 
-function App() {
+class App extends React.Component  {
+  render(){
   return (
     <div className="w-full h-auto bg-bodyColor text-lightText px-4">
         <Navbar/>
     <div className="max-w-screen-xl mx-auto">
+    <Helmet>
+                <meta charSet="utf-8" />
+                <title> Lawson Matutu Portfolio Website</title>
+                <link rel="canonical" href="https://symphonious-axolotl-8f4bde.netlify.app/" />
+            </Helmet>
+
+
   
     <Banner/>
   <Features/>
@@ -27,5 +36,5 @@ function App() {
     </div>
   );
 }
-
+}
 export default App;
