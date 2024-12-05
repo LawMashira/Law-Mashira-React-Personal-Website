ce33237feeb3593fed46f8dd20e074707c63e573
Lawson Matutu <lawsonmatutu@gmail.com>
2024-12-05 14:04:09 +0200

    updates

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -53,7 +53,7 @@ export default function Banner() {
           </h4>
           <h1 className="text-5xl font-bold text-white">
             I'm
-            <span className="text-designColor capitalize"> Law Mashira </span>
+            <span className="text-designColor capitalize"> Lawson Matutu </span>
           </h1>
           <h2 className="text-4xl font-bold text-white">
             a <span> {text} </span>
